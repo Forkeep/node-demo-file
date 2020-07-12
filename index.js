87458fe08f60e835bcf86e384b0f73a0a822347d
Forkeep <13126593533@163.com>
2020-07-12 23:40:14 +0800

    初识话项目

---
+++ index.js
@@ -0,0 +1,14 @@
+const program = require('commander');
+const HOME = process.env['HOME'];
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+if (program.debug) console.log(program.opts());
+if (program.small) console.log('- small pizza size');
+if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+
+console.log(HOME)
