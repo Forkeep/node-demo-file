fe4818f63c1ffb11f2a51da054880d61b48e0b4c
Forkeep <13126593533@163.com>
2020-07-13 23:21:22 +0800

    commander.js学习

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const program = require('commander');
-const HOME = process.env['HOME'];
+const {program} = require('commander');
+const pkg = require('./package.json');
+program.version(pkg.version);
+
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+  .option('-a, --add', 'add a new task')
+  .option('-c, --clear', 'clear all tasks');
 
 program.parse(process.argv);
 
-if (program.debug) console.log(program.opts());
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.add) {
+  console.log(...program.args);
+  console.log(process.argv.length)
+}
 
-console.log(HOME)
+if (program.clear) {
+  console.log(process.argv.length)
+}
+
+if (process.argv.length === 2){
+  console.log('显示所有任务列表')
+}
