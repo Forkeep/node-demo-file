c2269e6b3f3e4ee61ec26b2053b651a39693c443
Forkeep <13126593533@163.com>
2020-07-14 09:02:03 +0800

    数据库接口api

--- database.js
+++ database.js
@@ -7,11 +7,29 @@ const dbPath = p.join(home,'.todo');
 
 const db = {
   read(path = dbPath){
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, {flag: 'a+'}, (error, data) => {
+        if (error) return reject(error);
+        try {
+          list = JSON.parse(data.toString())
+        } catch (e) {
+          list = []
+        }
+        resolve(list)
+      })
+    })
 
   },
-  write(list,path = dbPath){
-
+  write(list, path = dbPath) {
+    return new Promise((resolve, reject) => {
+      const data = JSON.stringify(list);
+      fs.writeFile(path, data + '\n', error => {
+        if (error) return reject(error);
+        resolve()
+      })
+    })
   }
 };
+
 
 module.exports = db;
